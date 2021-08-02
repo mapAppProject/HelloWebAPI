0709b9d453bbbd4047cd724d6d80ed072ad45f23
t-akiba <akiba.tomohiro@tm2.co.jp>
2021-08-02 11:40:20 +0900

    test2

--- HelloWebAPI/Controllers/EmployeesController.cs
+++ HelloWebAPI/Controllers/EmployeesController.cs
@@ -9,10 +9,15 @@ namespace HelloWebAPI.Controllers
 {
     public class EmployeesController : ApiController
     {
+
         // GET api/<controller>
-        public IEnumerable<string> Get()
+        public IEnumerable<Employee> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new[]
+            {
+                new Employee{Id = 1, Name = "山田 太郎", BirthDay = DateTime.Parse("1970/01/01")},
+                new Employee{Id = 2, Name = "佐藤 花子", BirthDay = DateTime.Parse("2000/10/11")}
+            };
         }
 
         // GET api/<controller>/5
